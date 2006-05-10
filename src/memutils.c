08b7204b3d4a019ca40712e7d363159a890481a8
ken <ken>
2006-05-10 09:54:34 +0000

    Tidied up comments

--- src/memutils.c
+++ src/memutils.c
@@ -1,11 +1,13 @@
 /* :tabSize=4:indentSize=4:folding=indent:
- * $Id: memutils.c,v 1.1 2005/07/25 22:41:48 ken Exp $
+ * $Id: memutils.c,v 1.2 2006/05/10 09:54:34 ken Exp $
  */
 #include <stdlib.h>
 #include "memutils.h"
 #include "Logging.h"
 
-/* malloc() or die! */
+/* mu__malloc: Wrapper around malloc(); if malloc() returns NULL, terminate 
+ * the program
+ */
 void *mu__malloc(char *fileName, int lineNo, size_t size){
 	void *tmp = NULL;
 	
@@ -16,6 +18,9 @@ void *mu__malloc(char *fileName, int lineNo, size_t size){
 }
 
 
+/* mu__realloc: Wrapper around realloc(); if realloc() returns NULL, terminate 
+ * the program
+ */
 void *mu__realloc(char *fileName, int lineNo, void *ptr, size_t size){
 	void *tmp = NULL;
 	
@@ -26,6 +31,9 @@ void *mu__realloc(char *fileName, int lineNo, void *ptr, size_t size){
 }
 
 
+/* mu_free: Check pointer is not NULL before free()'ing it (this can screw up 
+ * many impelementations)
+ */
 void mu_free(void *ptr){
 	if(ptr != NULL) free(ptr);
 }

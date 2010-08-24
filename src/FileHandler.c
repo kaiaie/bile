21cdff2ed382c2bef8b1d49443883a1d6bb1c3fa
ken <ken>
2010-08-24 21:28:16 +0000

    Updated documentation comments

--- src/FileHandler.c
+++ src/FileHandler.c
@@ -1,5 +1,5 @@
 /* :tabSize=4:indentSize=4:folding=indent:
- * $Id: FileHandler.c,v 1.8 2006/06/10 20:23:42 ken Exp $
+ * $Id: FileHandler.c,v 1.9 2010/08/24 21:28:16 ken Exp $
  */
 #include <errno.h>
 #include <stdlib.h>
@@ -11,9 +11,12 @@
 #include "memutils.h"
 #include "path.h"
 
-FileHandler *new_FileHandler(bool (*canHandle)(char *fileName), 
-		void (*readMetadata)(char *fileName, Vars *vars), 
-		WriteStatus (*writeFile)(char *fileName, WriteFormat format, FILE *output)){
+/** Creates a new FileHandler */
+FileHandler *new_FileHandler(
+	bool (*canHandle)(char *fileName), 
+	void (*readMetadata)(char *fileName, Vars *vars), 
+	WriteStatus (*writeFile)(char *fileName, WriteFormat format, FILE *output)
+) {
 	FileHandler *result = NULL;
 	
 	result = (FileHandler *)mu_malloc(sizeof(FileHandler));
@@ -24,22 +27,28 @@ FileHandler *new_FileHandler(bool (*canHandle)(char *fileName),
 }
 
 
-void delete_FileHandler(FileHandler *fh){
-	if(fh != NULL){
+/** Deletes a FileHandler */
+void delete_FileHandler(FileHandler *fh) {
+	if (fh != NULL) {
 		mu_free(fh);
 	}
-	else{
+	else {
 		Logging_warnf("%s(): NULL argument", __FUNCTION__);
 	}
 }
 
 
-bool defaultCanHandle(char *fileName){
+/** Default handler; always returns True as the defaultReadMetadata function 
+*** can handle any file type
+**/
+bool defaultCanHandle(char *fileName) {
 	return true;
 }
 
 
-void defaultReadMetadata(char *fileName, Vars *vars){
+/** Default metadata reader; extracts file's name, size and modificaation date 
+**/
+void defaultReadMetadata(char *fileName, Vars *vars) {
 	struct stat st;
 	char *tmpFileName = NULL;
 	char *tmpFileSize = NULL;
@@ -47,7 +56,7 @@ void defaultReadMetadata(char *fileName, Vars *vars){
 	
 	tmpFileName = getPathPart(fileName, PATH_FILE);
 	Vars_let(vars, "file_name", tmpFileName, VAR_STD);
-	if(stat(fileName, &st) != -1){
+	if (stat(fileName, &st) != -1) {
 		tmpFileSize = asprintf("%d", st.st_size);
 		tmpFileDate = asprintf("%d", st.st_mtime);
 		Vars_let(vars, "file_size", tmpFileSize, VAR_STD);
@@ -55,7 +64,7 @@ void defaultReadMetadata(char *fileName, Vars *vars){
 		mu_free(tmpFileDate);
 		mu_free(tmpFileSize);
 	}
-	else{
+	else {
 		Logging_warnf("%s: Unable to stat() file \"%s\": %s", 
 				__FUNCTION__, fileName, strerror(errno));
 	}
@@ -63,6 +72,7 @@ void defaultReadMetadata(char *fileName, Vars *vars){
 }
 
 
+/** Default file writer function */
 WriteStatus defaultWriteOutput(char *fileName, WriteFormat format, FILE *output){
 	char base64Alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 	unsigned char base64Buffer[3];
@@ -73,57 +83,67 @@ WriteStatus defaultWriteOutput(char *fileName, WriteFormat format, FILE *output)
 	FILE        *in     = NULL;
 	int currChr;
 	
-	if((in = fopen(fileName, "rb")) != NULL){
-		if(format == WF_BASE64){
-			while(!feof(in)){
+	if ((in = fopen(fileName, "rb")) != NULL) {
+		if (format == WF_BASE64) {
+			while (!feof(in)) {
 				base64Buffer[0] = 0; base64Buffer[1] = 0; base64Buffer[2] = 0;
 				bytesRead = fread(base64Buffer, sizeof(char), 3, in);
 				idx = base64Buffer[0] >> 2;
 				fputc(base64Alphabet[idx], output);
 				idx = ((base64Buffer[0] & 0x03) << 4) | (base64Buffer[1] >> 4);
 				fputc(base64Alphabet[idx], output);
-				if(bytesRead == 2)
+				if(bytesRead == 2) {
 					idx = 64;
-				else
+				}
+				else {
 					idx = ((base64Buffer[1] & 0x0f) << 2) | (base64Buffer[2] >> 6);
+				}
 				fputc(base64Alphabet[idx], output);
-				if(bytesRead <= 2)
+				if (bytesRead <= 2) {
 					idx = 64;
-				else
+				}
+				else {
 					idx = base64Buffer[2] & 0x3f;
+				}
 				fputc(base64Alphabet[idx], output);
 				lineLength += 4;
-				if(lineLength == 76){
+				if (lineLength == 76) {
 					fputc('\n', output);
 					lineLength = 0;
 				}
 			}
 			fputc('\n', output);
 		}
-		else if(format == WF_SGMLENCODE){
-			while((currChr = fgetc(in)) != EOF){
-				if(currChr == '&')
+		else if (format == WF_SGMLENCODE) {
+			while((currChr = fgetc(in)) != EOF) {
+				if(currChr == '&') {
 					fputs("&amp;", output);
-				else if(currChr == '<')
+				}
+				else if(currChr == '<') {
 					fputs("&lt;", output);
-				else if(currChr == '>')
+				}
+				else if(currChr == '>') {
 					fputs("&gt;", output);
-				else if(currChr == '\"')
+				}
+				else if(currChr == '\"') {
 					fputs("&quot;", output);
-				else
+				}
+				else {
 					fputc(currChr, output);
+				}
 			}
 		}
-		else{
+		else {
 			/* Default: verbatim output.  The default handler never returns 
 			 * WS_UNSUPPORTED.
 			 */
-			while((currChr = fgetc(in)) != EOF)
+			 while ((currChr = fgetc(in)) != EOF) {
 				fputc(currChr, output);
+			 }
 		}
 		fclose(in);
 	}
-	else{
+	else {
 		Logging_warnf("%s: Error opening file \"%s\": %s", 
 				__FUNCTION__, fileName, strerror(errno));
 		result = WS_ERROR;
--- src/FileHandler.h
+++ src/FileHandler.h
@@ -1,66 +1,87 @@
 /* :tabSize=4:indentSize=4:folding=indent:
- * $Id: FileHandler.h,v 1.4 2006/05/10 15:01:18 ken Exp $
- * FileHandler - BILE uses a Dict of FileHandler structures to process files of 
- * a particular type.  When BILE encounters a file, it calls the canHandle() 
- * function of each FileHandler and passes the filename.  The canHandle() 
- * function decides whether the particular handler can extract meaningful 
- * metadata from files of this type.  If the handler's canHandle() function 
- * returns True, BILE will call the readMetadata() function of each responding 
- * handler to build the file's metadata.  When it comes to write the output 
- * file, it calls the writeFile() function of each supporting handler until 
- * one of them returns OK or COPYORIGINAL.
- *
- * The corresponding C file contains the canHandle(), readMetadata(), and 
- * writeFile() implementations for the default file handler.  It extracts the 
- * basic metadata (e.g. file size, modification date, etc.) and implements the 
- * VERBATIM, BASE64 and SGMLENCODE output methods.
- */
+** $Id: FileHandler.h,v 1.5 2010/08/24 21:28:16 ken Exp $
+*/
+/**
+*** \file FileHandler.h
+*** \brief Generic file handler
+***
+*** BILE uses a Dict of FileHandler structures to process files of 
+*** a particular type.  When BILE encounters a file, it calls the canHandle() 
+*** function of each FileHandler and passes the filename.  The canHandle() 
+*** function decides whether the particular handler can extract meaningful 
+*** metadata from files of this type.  If the handler's canHandle() function 
+*** returns True, BILE will call the readMetadata() function of each responding 
+*** handler to build the file's metadata.  When it comes to write the output 
+*** file, it calls the writeFile() function of each supporting handler until 
+*** one of them returns OK or COPYORIGINAL.
+***
+*** The corresponding C file contains the canHandle(), readMetadata(), and 
+*** writeFile() implementations for the default file handler.  It extracts the 
+*** basic metadata (e.g. file size, modification date, etc.) and implements the 
+*** VERBATIM, BASE64 and SGMLENCODE output methods.
+**/
 #ifndef FILEHANDLER_H
 #define FILEHANDLER_H
 #include <stdio.h>
 #include "bool.h"
 #include "Vars.h"
 
-/* How the file's contents should be formatted when written to the output.  
- * A handler is not required to implement all these formats; if its writeFile()
- * function returns UNSUPPORTED, BILE will try another handler until it finds 
- * one that will.
- * VERBATIM     = copy the content of the file into the template
- * BASE64       = base64-encode the content before copying it
- * SGMLENCODE   = escape those characters (&, <, >, ") meaningful to SGML
- * HTMLPREAMBLE = assume input is HTML and copy the content prior to the HTML 
- *                tag (for e.g. PHP or ASP code that must run before anything 
- *                is emitted)
- * HTMLBODY     = assume input is HTML and copy the part of the file in the 
- *                <BODY> element.
- * HTMLLINK     = Write an HTML <A> element pointing to the file.
- * HTMLINLINE   = Write an HTML <IMG> or <OBJECT> element
- */
-typedef enum {WF_VERBATIM, WF_BASE64, WF_SGMLENCODE, WF_HTMLPREAMBLE, WF_HTMLBODY, WF_HTMLLINK, 
-		WF_HTMLINLINE} WriteFormat;
+/** \brief How the file's contents should be formatted when written to the 
+*** output.
+***
+*** A handler is not required to implement all these formats; if its writeFile()
+*** function returns UNSUPPORTED, BILE will try another handler until it finds 
+*** one that will.
+**/
+typedef enum {
+	/** Copy the content of the file into the template without changes */
+	WF_VERBATIM, 
+	/** base64-encode the content before copying it */
+	WF_BASE64,
+	/** Escape those characters (\&, \<, \>, \") meaningful to SGML */
+	WF_SGMLENCODE, 
+	/** Assume input is HTML and copy the content prior to the HTML
+	*** tag (for e.g. PHP or ASP code that must run before anything
+	*** is emitted)
+	**/
+	WF_HTMLPREAMBLE,
+	/** assume input is HTML and copy the part of the file in the 
+	*** \<BODY\> element
+	**/
+	WF_HTMLBODY,
+	/** Write an HTML \<A\> element pointing to the file */
+	WF_HTMLLINK, 
+	/** Write an HTML \<IMG\> or \<OBJECT\> element */
+	WF_HTMLINLINE
+} WriteFormat;
 
-/* Return code for the writeFile() function:
- * ERROR        = something bad happened
- * OK           = body written succesfully
- * COPYORIGINAL = success, but copy original file (for when a link to the file 
- *                rather than its actual contents is written)
- * UNSUPPORTED  = this function doesn't support the requested format; try 
- *                another handler.
- */
-typedef enum {WS_ERROR = -1, WS_OK, WS_COPYORIGINAL, WS_UNSUPPORTED} 
-		WriteStatus;
+/** Return code for the writeFile() function: */
+typedef enum {
+	/** Something bad happened */
+	WS_ERROR = -1, 
+	/** Body written successfully */
+	WS_OK,
+	/** Success, but copy original file (for when a link to the file rather 
+	*** than its actual contents is written)
+	**/
+	WS_COPYORIGINAL,
+	/** This function doesn't support the requested format; try another handler
+	**/
+	WS_UNSUPPORTED
+} WriteStatus;
 
-typedef struct _filehandler{
+typedef struct tag_filehandler{
 	bool (*canHandle)(char *fileName);
 	void (*readMetadata)(char *fileName, Vars *vars);
 	WriteStatus (*writeFile)(char *fileName, WriteFormat format, FILE *output);
 } FileHandler;
 
+FileHandler *new_FileHandler(
+	bool (*canHandle)(char *fileName), 
+	void (*readMetadata)(char *fileName, Vars *data), 
+	WriteStatus (*writeFile)(char *fileName, WriteFormat format, FILE *output)
+);
 
-
-FileHandler *new_FileHandler(bool (*canHandle)(char *fileName), 
-		void (*readMetadata)(char *fileName, Vars *data), 
-		WriteStatus (*writeFile)(char *fileName, WriteFormat format, FILE *output));
 void delete_FileHandler(FileHandler *fh);
 
 /* Default implementations of handler functions */

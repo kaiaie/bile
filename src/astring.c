e45d0f837e5fec25b453294c73bd7c3bc3419c7a
ken <ken>
2010-08-24 10:35:05 +0000

    Fixed segmentation violation, added documentation comments

--- src/astring.c
+++ src/astring.c
@@ -1,5 +1,5 @@
 /* :tabSize=4:indentSize=4:
- * $Id: astring.c,v 1.12 2010/07/08 22:16:15 ken Exp $
+ * $Id: astring.c,v 1.13 2010/08/24 10:35:05 ken Exp $
  */
 #include <ctype.h>
 #include <stdio.h>
@@ -15,19 +15,17 @@
 #define vsnprintf _vsnprintf
 #endif
 
-/**
- * Returns a copy of the string passed to it
- *
- * (equivalent to strdup() found in some standard libraries)
- */
+/** Returns a copy of the string passed to it (equivalent to strdup() found in 
+*** some standard libraries)
+**/
 char *astrcpy(const char *src){
 	char *inputCopy = NULL;
 	
-	if(src == NULL){
+	if (src == NULL){
 		Logging_warnf("%s(): Trying to copy NULL string!", __FUNCTION__);
 		inputCopy = astrcpy("");
 	}
-	else{
+	else {
 		inputCopy = (char *)mu_malloc((strlen(src) + 1) * sizeof(char));
 		strcpy(inputCopy, src);
 	}
@@ -35,21 +33,27 @@ char *astrcpy(const char *src){
 }
 
 
-char *astrcat(const char *src1, const char *src2){
-	/* Returns a new string comprising of the two strings passed */
-	/* to it joined together.                                    */
+/** Returns a new string comprising of the two strings passed to it joined 
+*** together
+**/
+char *astrcat(const char *src1, const char *src2) {
 	char   *catString = NULL;
-	size_t catLength  = strlen(src1) + strlen(src2) + 1;
+	size_t catLength;
 	
-	if(src1 == NULL){
+	if (src1 == NULL && src2 == NULL) {
+		Logging_warnf("%s(): Arguments are NULL!", __FUNCTION__);
+		catString = astrcpy("");
+	}
+	else if (src1 == NULL && src2 != NULL) {
 		Logging_warnf("%s(): First argument is NULL!", __FUNCTION__);
 		catString = astrcpy(src2);
 	}
-	else if(src2 == NULL){
+	else if (src2 == NULL && src1 != NULL) {
 		Logging_warnf("%s(): Second argument is NULL!", __FUNCTION__);
 		catString = astrcpy(src1);
 	}
-	else{
+	else {
+		catLength = strlen(src1) + strlen(src2) + 1;
 		catString = (char *)mu_malloc(catLength * sizeof(char));
 		strcpy(catString, src1);
 		strcat(catString, src2);
@@ -58,25 +62,25 @@ char *astrcat(const char *src1, const char *src2){
 }
 
 
+/** Returns a new string consisting of the leftmost count characters of the 
+*** source string; equivalent to BASIC Left$() function
+**/
 char *astrleft(const char *src, size_t count){
-	/* Returns a new string consisting of the leftmost count          */
-	/* characters of the source string; equivalent to BASIC           */
-	/* Left$() function                                               */
 	char   *leftString = NULL;
 	size_t actualCount;
 	
-	if(count == 0){
+	if (count == 0) {
 		return astrcpy("");
 	}
-	else if(count >= strlen(src)){
+	else if (count >= strlen(src)) {
 		return astrcpy(src);
 	}
-	else{
+	else {
 		leftString = (char *)mu_malloc((count + 1) * sizeof(char));
-		if(count >= strlen(src)){
+		if (count >= strlen(src)) {
 			actualCount = strlen(src);
 		}
-		else{
+		else {
 			actualCount = count;
 		}
 		memcpy(leftString, src, actualCount);
@@ -86,22 +90,22 @@ char *astrleft(const char *src, size_t count){
 }
 
 
+/** Returns a new string consisting of the count characters starting at offset 
+*** in the source string.  If count is zero, all characters to the end of the 
+*** string are returned.  Equivalent to BASIC Mid$() function.                
+**/
 char *astrmid(const char *src, size_t offset, size_t count){
-	/* Returns a new string consisting of the count characters        */
-	/* starting at offset in the source string.                       */
-	/* If count is zero, all characters to the end of the string are  */
-	/* returned.  Equivalent to BASIC Mid$() function.                */
 	char *midString = NULL;
 	
 	size_t actualCount;
-	if(offset >= strlen(src)){
+	if (offset >= strlen(src)) {
 		return NULL;
 	}
-	else{
-		if((count == 0) || (offset + count > strlen(src))){
+	else {
+		if ((count == 0) || (offset + count > strlen(src))) {
 			actualCount = strlen(src) - offset;
 		}
-		else{
+		else {
 			actualCount = count;
 		}
 		midString = (char *)mu_malloc((actualCount + 1) * sizeof(char));
@@ -112,10 +116,11 @@ char *astrmid(const char *src, size_t offset, size_t count){
 }
 
 
+/** Returns a new string consisting of the rightmost count characters of the 
+*** source string.  Equivalent to BASIC Right$() function
+**/
 char *astrright(const char *src, size_t count){
-	/* Returns a new string consisting of the rightmost count         */
-	/* characters of the source string.  Equivalent to BASIC Right$() */
-	if(count >= strlen(src)){
+	if (count >= strlen(src)) {
 		return astrcpy(src);
 	}
 	else{
@@ -124,6 +129,9 @@ char *astrright(const char *src, size_t count){
 }
 
 
+/** Similar to sprintf() except that the result is returned in a dynamically 
+*** allocated string
+**/
 char *asprintf(const char *fmt, ...){
 	size_t bufferLength = 64;
 	size_t newLength;
@@ -132,21 +140,21 @@ char *asprintf(const char *fmt, ...){
 	
 	buffer = (char *)mu_malloc(bufferLength * sizeof(char));
 	va_start(ap, fmt);
-	do{
-		if(vsnprintf(buffer, bufferLength, fmt, ap) >= bufferLength){
+	do {
+		if (vsnprintf(buffer, bufferLength, fmt, ap) >= bufferLength) {
 			newLength = bufferLength * 2;
 			buffer = (char *)mu_realloc(buffer, newLength * sizeof(char));
 			bufferLength = newLength;
 		}
 		else break;
-	} while(true);
+	} while (true);
 	va_end(ap);
 	return buffer;
 }
 
 
-char *astrfillch(size_t len, char c){
-	/* Returns a string consisting of len occurrences of c. */
+/** Returns a string consisting of len occurrences of c. */
+char *astrfillch(size_t len, char c) {
 	char *buffer = NULL;
 	size_t ii;
 	
@@ -159,19 +167,22 @@ char *astrfillch(size_t len, char c){
 }
 
 
+/** Returns a string of length len consisting of as many repetitions of the 
+*** string s as will fit
+**/
 char *astrfill(size_t len, const char *s){
 	char   *result = NULL;
 	size_t ii, jj = 0;
 	
-	if(strlen(s) <= 0){
+	if (strlen(s) <= 0) {
 		Logging_fatalf("%s: Illegal argument.", __FUNCTION__);
 	}
-	else if(strlen(s) == 1){
+	else if (strlen(s) == 1) {
 		result = astrfillch(len, s[0]);
 	}
-	else{
+	else {
 		result = (char *)mu_malloc((len + 1) * sizeof(char));
-		for(ii = 0; ii < len; ++ii){
+		for (ii = 0; ii < len; ++ii) {
 			result[ii] = s[jj];
 			result[ii + 1] = '\0';
 			jj = (jj + 1) % strlen(s);
@@ -181,63 +192,64 @@ char *astrfill(size_t len, const char *s){
 }
 
 
-char *astrunquote(const char *s){
-	/* Returns a string consisting of a copy of s with the first and last 
-	 * characters removed.  Does not actually check if said characters are 
-	 * quotes before doing so!
-	 */
-	if(strlen(s) == 2) return astrcpy("");
+/** Returns a string consisting of a copy of s with the first and last 
+*** characters removed.  Does not actually check if said characters are 
+*** quotes before doing so!
+**/
+char *astrunquote(const char *s) {
+	if (strlen(s) == 2) return astrcpy("");
 	else return astrmid(s, 1, strlen(s) - 2);
 }
 
 
-char *astrupper(const char *s){
-	/* Returns a string consisting of a copy of s with all characters converted 
-	 * to upper case.
-	 */
+/** Returns a string consisting of a copy of s with all characters converted 
+*** to upper case.
+**/
+char *astrupper(const char *s) {
 	return strxupper(astrcpy(s));
 }
 
 
-char *astrlower(const char *s){
-	/* Returns a string consisting of a copy of s with all characters converted 
-	 * to lower case.
-	 */
+/** Returns a string consisting of a copy of s with all characters converted 
+*** to lower case.
+**/
+char *astrlower(const char *s) {
 	return strxlower(astrcpy(s));
 }
 
 
+/** Returns a string consisting of a copy of the directory part of path.  
+*** Does not change its argument.  Backslashes are converted into forward 
+*** slashes.
+*** \note A version of this function exists in libiberty but the doc is very 
+*** ambiguous about when and if it alters its input.
+**/
 char *adirname(const char *path){
-	/* Returns a string consisting of a copy of the directory part of path.  
-	 * Does not change path.  Backslashes are converted into forward slashes.
-	 * A version of this function exists in libiberty but the doc is very 
-	 * ambiguous about when and if it alters its input.
-	 */
 	char *result = NULL;
 	char *tmp    = NULL;
 	size_t ii;
 	
-	if(strxnullorempty(path)){
+	if (strxnullorempty(path)) {
 		result = astrcpy(".");
 	}
-	else{
+	else {
 		tmp = strxreplace(astrcpy(path), '\\', '/');
-		if(strchr(tmp, '/') == NULL){
+		if (strchr(tmp, '/') == NULL) {
 			result = astrcpy(".");
 		}
-		else if(strxequals(tmp, "/") || strxequals(tmp, ".") || strxequals(tmp, "..")){
+		else if (strxequals(tmp, "/") || strxequals(tmp, ".") || strxequals(tmp, "..")) {
 			result = astrcpy(tmp);
 		}
-		else{
-			if(tmp[strlen(tmp) - 1] == '/') tmp[strlen(tmp) - 1] = '\0';
+		else {
+			if (tmp[strlen(tmp) - 1] == '/') tmp[strlen(tmp) - 1] = '\0';
 		}
 		ii = strlen(tmp);
-		while(ii >= 0){
-			if(tmp[ii] == '/'){
-				if(ii == 0){
+		while (ii >= 0) {
+			if (tmp[ii] == '/') {
+				if (ii == 0) {
 					result = astrcpy("/");
 				}
-				else{
+				else {
 					result = astrleft(tmp, ii - 1);
 				}
 				break;
@@ -250,10 +262,10 @@ char *adirname(const char *path){
 }
 
 
+/** Returns a string consisting of a copy of the file part of path.  
+*** Does not change path.  Backslashes are converted into forward slashes.
+**/
 char *abasename(const char *path){
-	/* Returns a string consisting of a copy of the file part of path.  
-	 * Does not change path.  Backslashes are converted into forward slashes.
-	 */
 	char *result = NULL;
 	char *tmp    = NULL;
 	size_t ii;
@@ -286,10 +298,10 @@ char *abasename(const char *path){
 }
 
 
+/** Breaks the string s where it encounters any of the characters in delims.
+*** Returns a NULL-terminated array of strings.
+***/
 char **astrtok(const char *s, const char *delims){
-	/* Breaks the string s where it encounters any of the characters in delims.
-	 * Returns a NULL-terminated array of strings.
-	 */
 	char   **result = NULL;
 	size_t ii, jj;
 	size_t last;
@@ -337,17 +349,18 @@ char **astrtok(const char *s, const char *delims){
 }
 
 
-/* alength: Returns the length of a NULL-terminated string */
+/** Returns the length of a NULL-terminated string array */
 size_t alength(char **a){
 	size_t result = 0;
-	while(a[result] != NULL) result++;
+	if (a != NULL) {
+		while (a[result] != NULL) result++;
+	}
 	return result;
 }
 
 
+/** Frees the strings and array returned by astrtok() */
 void astrtokfree(char **l){
-	/* Frees the strings and array returned by astrtok()
-	 */
 	size_t ii = 0;
 	
 	if(l != NULL){
@@ -360,6 +373,7 @@ void astrtokfree(char **l){
 }
 
 
+/** Returns a copy of the string with characters in reverse order */
 char *astrrev(const char *s){
 	char *result = NULL;
 	size_t ii;
@@ -373,12 +387,13 @@ char *astrrev(const char *s){
 }
 
 
+/** Returns a copy of the string with leading whitespace removed */
 char *astrltrim(const char *s){
 	char   *result = NULL;
 	size_t ii;
 	
-	for(ii = 0; ii < strlen(s); ++ii){
-		if(!isspace(s[ii])){
+	for (ii = 0; ii < strlen(s); ++ii) {
+		if (!isspace(s[ii])) {
 			result = astrmid(s, ii, 0);
 			break;
 		}
@@ -388,12 +403,13 @@ char *astrltrim(const char *s){
 }
 
 
+/** Returns a copy of the string with trailing whitespace removed */
 char *astrrtrim(const char *s){
 	char   *result = NULL;
 	size_t ii;
 	
-	for(ii = strlen(s) - 1; ii >= 0; --ii){
-		if(!isspace(s[ii])){
+	for (ii = strlen(s) - 1; ii >= 0; --ii) {
+		if (!isspace(s[ii])) {
 			result = astrleft(s, ii + 1);
 			break;
 		}
@@ -403,6 +419,7 @@ char *astrrtrim(const char *s){
 }
 
 
+/** Returns a copy of the string with leading and trailing whitespace removed */
 char *astrtrim(const char *s){
 	char *tmp1 = NULL;
 	char *tmp2 = NULL;
@@ -414,13 +431,14 @@ char *astrtrim(const char *s){
 }
 
 
+/** Returns a copy of string src up to a maximum length of n characters */
 char *astrncpy(const char *src, size_t n){
 	char *result = NULL;
 	
-	if(strlen(src) <= n){
+	if (strlen(src) <= n) {
 		return astrcpy(src);
 	}
-	else{
+	else {
 		result = (char *)mu_malloc((n + 1) * sizeof(char));
 		strncpy(result, src, n);
 		return result;

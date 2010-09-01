2dcb2da21f0771ca61f91ddd7ada5cf735aaf2bb
ken <ken>
2010-09-01 15:45:45 +0000

    Added icon, bumped version

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /* :tabSize=4:indentSize=4:folding=indent:
-** $Id: version.h,v 1.1 2010/08/31 15:53:14 ken Exp $
+** $Id: version.h,v 1.2 2010/09/01 15:45:45 ken Exp $
 */
 /** \file version.h
 *** \brief Build version information
@@ -9,7 +9,7 @@
 
 #define APP_VERSION_MAJOR 0
 #define APP_VERSION_MINOR 2
-#define APP_VERSION_BUILD 1
-#define APP_VERSION_STRING "0.2.0.1"
+#define APP_VERSION_BUILD 2
+#define APP_VERSION_STRING "0.2.0.2"
 
 #endif
